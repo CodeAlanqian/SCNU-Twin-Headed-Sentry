f80f7772e04dcef395c511a71da3990ccef6baeb
CodeAlan <2546379375@qq.com>
2024-04-06 21:53:14 +0800

    Add rm_decision

---
+++ src/Twin-Headed-Sentry-Nav/rm_decision/include/decision.hpp
@@ -0,0 +1,59 @@
+#ifndef RM_DICISION__DECISION_HPP_
+#define RM_DICISION__DECISION_HPP_
+
+#include <rclcpp/rclcpp.hpp>
+#include <rclcpp/publisher.hpp>
+#include <rclcpp/subscription.hpp>
+#include <rclcpp_action/rclcpp_action.hpp>
+
+//nav2
+#include <nav2_msgs/action/navigate_to_pose.hpp>
+#include <geometry_msgs/msg/pose_stamped.hpp>
+#include <geometry_msgs/msg/point.hpp>
+
+#include <thread>
+//msg from sentry
+#include "auto_aim_interfaces/msg/from_sentry.hpp"
+namespace rm_decision
+{
+
+class RMDecision : public rclcpp::Node
+{
+public:
+    explicit RMDecision(const rclcpp::NodeOptions & options);
+    ~RMDecision();
+private:
+    rclcpp::Subscription<auto_aim_interfaces::msg::FromSentry>::SharedPtr from_sentry_sub_;
+    void fromSentryCallback(const auto_aim_interfaces::msg::FromSentry::SharedPtr msg);
+    void decision();
+    void sendGoal();
+    void goalResultCallback(const rclcpp_action::ClientGoalHandle<nav2_msgs::action::NavigateToPose>::WrappedResult& result);
+    rclcpp_action::Client<nav2_msgs::action::NavigateToPose>::SharedPtr action_client_;
+
+    nav2_msgs::action::NavigateToPose::Goal goal_msg_;
+    rclcpp_action::Client<nav2_msgs::action::NavigateToPose>::SendGoalOptions send_goal_options_;
+
+    uint16_t hp_;
+    uint16_t time_;
+    rclcpp_action::ClientGoalHandle<nav2_msgs::action::NavigateToPose>::WrappedResult goal_result_;
+    // float point_home_ = {0.0, 0.0};
+    // float point_center_ = {3.0, 1.0};
+    // float point_supplyArea_ = {-2.5, 4.07};
+    // float point_enemySupplyArea_ = {5.3, -1.35};
+    geometry_msgs::msg::Point point_home_;
+    geometry_msgs::msg::Point point_center_;
+    geometry_msgs::msg::Point point_supply_area_;
+    geometry_msgs::msg::Point point_enemy_supply_area_;
+    geometry_msgs::msg::Point point_wall_;
+
+    std::thread receive_thread_;
+    /* data */
+};
+
+
+
+}  // namespace rm_decision
+
+
+
+#endif  // RM_DICISION__DECISION_HPP_
---
+++ src/Twin-Headed-Sentry-Nav/rm_decision/src/decision.cpp
@@ -0,0 +1,182 @@
+#include <rclcpp/logging.hpp>
+#include <rclcpp/qos.hpp>
+#include <rclcpp/utilities.hpp>
+
+#include "decision.hpp"
+
+using namespace std::chrono_literals;
+
+namespace rm_decision
+{
+RMDecision::RMDecision(const rclcpp::NodeOptions & options)
+: Node("rm_decision", options)
+{
+    RCLCPP_INFO(this->get_logger(), "RMDecision node has been started.");
+    from_sentry_sub_ = this->create_subscription<auto_aim_interfaces::msg::FromSentry>(
+        "/from_sentry", 10, std::bind(&RMDecision::fromSentryCallback, this, std::placeholders::_1));
+    action_client_ = rclcpp_action::create_client<nav2_msgs::action::NavigateToPose>(this, "navigate_to_pose");
+    
+    goal_msg_.pose.header.frame_id = "map";
+    goal_msg_.pose.pose.position.x = 0.0;
+    goal_msg_.pose.pose.position.y = 0.0;
+    goal_msg_.pose.pose.position.z = 0.0;
+    goal_msg_.pose.pose.orientation.x = 0.0;
+    goal_msg_.pose.pose.orientation.y = 0.0;
+    goal_msg_.pose.pose.orientation.z = 0.0;
+    goal_msg_.pose.pose.orientation.w = 1.0;
+
+    
+    point_home_.x = 0.0;
+    point_home_.y = 0.0;
+
+    point_center_.x = 3.0;
+    point_center_.y = 1.0;
+
+    point_supply_area_.x = -2.5;
+    point_supply_area_.y = 2.85;
+    // -2.21, 2.85
+
+    point_wall_.x = 0.43;
+    point_wall_.y = 1.59;
+
+    point_enemy_supply_area_.x = 5.3;
+    point_enemy_supply_area_.y = -1.35;
+
+    hp_ = 0;
+    time_ = 0;
+
+    send_goal_options_.result_callback = std::bind(&RMDecision::goalResultCallback, this, std::placeholders::_1);
+    // action_client_->async_send_goal(goal_msg, send_goal_options);
+    receive_thread_ = std::thread(&RMDecision::decision, this);
+    // decision();
+}
+
+void RMDecision::fromSentryCallback(const auto_aim_interfaces::msg::FromSentry::SharedPtr msg)
+{
+    hp_ = msg->hp;
+    time_ = msg->time;
+    RCLCPP_INFO(this->get_logger(), "Received message from Sentry. hp:%d, time:%d", hp_, time_);
+}
+
+void RMDecision::goalResultCallback(const rclcpp_action::ClientGoalHandle<nav2_msgs::action::NavigateToPose>::WrappedResult& result)
+{
+    RCLCPP_INFO(this->get_logger(), "Goal has been reached.");
+    goal_result_ = result;
+    //goal_result_.code  =
+    //rclcpp_action::ResultCode::SUCCEEDED
+    //rclcpp_action::ResultCode::ABORTED
+    //rclcpp_action::ResultCode::CANCELED
+}
+
+
+void RMDecision::sendGoal()
+{
+    RCLCPP_INFO(this->get_logger(), "Sending goal.");
+    while (!action_client_->wait_for_action_server(10s)) {
+        if (!rclcpp::ok()) {
+            RCLCPP_ERROR(get_logger(), "Action server not available after waiting.");
+            return;
+        }
+        RCLCPP_INFO(get_logger(), "Waiting for action server to be available...");
+    }
+    
+    action_client_->async_send_goal(goal_msg_, send_goal_options_);
+}
+
+
+void RMDecision::decision()
+{
+    rclcpp::WallRate loop_rate(10.0);
+    while (rclcpp::ok())
+    {
+        RCLCPP_INFO(this->get_logger(), "Decision making.");
+        loop_rate.sleep();
+        if (time_ > 0)
+        {
+            if (hp_ == 600) {
+                goal_msg_.pose.pose.position.x = point_enemy_supply_area_.x;
+                goal_msg_.pose.pose.position.y = point_enemy_supply_area_.y;
+
+            } else if (hp_ <= 300) {
+                goal_msg_.pose.pose.position.x = point_supply_area_.x;
+                goal_msg_.pose.pose.position.y = point_supply_area_.y;
+            } else if (hp_ == 500 ) {
+                goal_msg_.pose.pose.position.x = point_center_.x;
+                goal_msg_.pose.pose.position.y = point_center_.y;
+            }
+            sendGoal();
+
+        }
+    }
+    
+    // RCLCPP_INFO(this->get_logger(), "Decision making.");
+    // if (goal_result_.code == rclcpp_action::ResultCode::SUCCEEDED)
+    // {
+    //     RCLCPP_INFO(this->get_logger(), "Goal has been reached.");
+    // }
+    // else if (goal_result_.code == rclcpp_action::ResultCode::ABORTED)
+    // {
+    //     RCLCPP_INFO(this->get_logger(), "Goal has been aborted.");
+    // }
+    // else if (goal_result_.code == rclcpp_action::ResultCode::CANCELED)
+    // {
+    //     RCLCPP_INFO(this->get_logger(), "Goal has been canceled.");
+    // }
+    // else
+    // {
+    //     RCLCPP_INFO(this->get_logger(), "Goal has not been reached.");
+    // }
+    
+    
+    // if (hp_ < 100)
+    // {
+    //     goal_msg_.pose.pose.position.x = point_home_[0];
+    //     goal_msg_.pose.pose.position.y = point_home_[1];
+    //     sendGoal();
+    // }
+    // else if (hp_ >= 100 && hp_ < 200)
+    // {
+    //     goal_msg_.pose.pose.position.x = point_center_[0];
+    //     goal_msg_.pose.pose.position.y = point_center_[1];
+    //     sendGoal();
+    // }
+    // else if (hp_ >= 200 && hp_ < 300)
+    // {
+    //     goal_msg_.pose.pose.position.x = point_supplyArea_[0];
+    //     goal_msg_.pose.pose.position.y = point_supplyArea_[1];
+    //     sendGoal();
+    // }
+    // else if (hp_ >= 300 && hp_ < 400)
+    // {
+    //     goal_msg_.pose.pose.position.x = point_enemySupplyArea_[0];
+    //     goal_msg_.pose.pose.position.y = point_enemySupplyArea_[1];
+    //     sendGoal();
+    // }
+    // else
+    // {
+    //     RCLCPP_INFO(this->get_logger(), "HP is too high.");
+    // }
+}
+
+RMDecision::~RMDecision()
+{
+    if (receive_thread_.joinable()) {
+        receive_thread_.join();
+    }
+    RCLCPP_INFO(this->get_logger(), "RMDecision node has been destroyed.");
+}
+
+
+
+
+} // namespace rm_decision
+
+int main(int argc, char * argv[])
+{
+    rclcpp::init(argc, argv);
+    auto node_options = rclcpp::NodeOptions(); 
+    auto node = std::make_shared<rm_decision::RMDecision>(node_options);
+    rclcpp::spin(node);
+    rclcpp::shutdown();
+    return 0;
+}
